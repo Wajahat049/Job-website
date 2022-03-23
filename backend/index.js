b0399f46f23f6c3c4bcadb128d36bdc30ebcc4fd
Wajahat Ahmed <64039031+Wajahat049@users.noreply.github.com>
2022-03-24 02:03:01 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,20 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const pretty = require("pretty");
-const fs = require('fs');
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+// const pretty = require("pretty");
+// const fs = require('fs');
 
-let rawdata = fs.readFileSync("E:\\job-search-website\\src\\Assests\\1,144 Computer Jobs in Karachi - 22 March, 2022 _ Indeed.com Pakistan(1).json");
-let data = JSON.parse(rawdata);
-for(var i=0;i<data.length;i++){
-console.log(data[i].Field2);
-console.log(data[i].Field4);
-console.log(data[i].Field5);
-console.log("");
-
-
+// let rawdata = fs.readFileSync("E:\\job-search-website\\src\\Assests\\1,144 Computer Jobs in Karachi - 22 March, 2022 _ Indeed.com Pakistan(1).json");
+// let data = JSON.parse(rawdata);
+// for(var i=0;i<data.length;i++){
+// console.log(data[i].Field2);
+// console.log(data[i].Field4);
+// console.log(data[i].Field5);
+// console.log("");
 
 
-}
+
+
+// }
 
 // const url = "https://pk.indeed.com/m/jobs?q=Computer&l=karachi";
 
@@ -32,3 +32,26 @@ console.log("");
 // }
 
 // Scrape()
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+async function getData(){
+
+const uri = "mongodb+srv://wajahat:wajju123@cluster0.0buc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+let result =await client.connect()
+// client.connect(async err =>{
+  let collection = result.db("GoJobber").collection("Users");
+  collection.insertOne({name:"ahmed"}, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    // db.close();
+  });
+//   let response = await collection.find({}).toArray()
+//   console.log(response[0].name)
+  // perform actions on the collection object
+//   client.close();
+// })
+}
+getData()
