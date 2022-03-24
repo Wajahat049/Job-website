8053fd86f5aa5941b29c878291015ba8fdd99efb
Wajahat Ahmed <64039031+Wajahat049@users.noreply.github.com>
2022-03-25 00:35:00 +0500

    Add files via upload

--- src/Components/accountBox/signupForm.jsx
+++ src/Components/accountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useEffect, useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -12,17 +12,41 @@ import { AccountContext } from "./accountContext";
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [pass,setPass] = useState("")
+
+
+
+
+  useEffect(()=>{
+   
+  })
+
+
+  const Submit=()=>{
+    console.log(name,email,pass)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name,email,pass })
+  };
+  fetch('http://localhost:8000/CreateUser', requestOptions)
+
+  }
+
+
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)} />
+        <Input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+        <Input type="password" placeholder="Password" onChange={(e)=>setPass(e.target.value)} />
         <Input type="password" placeholder="Confirm Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton onClick={()=>Submit()} type="submit">Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
