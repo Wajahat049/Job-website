ba7f93f067e97af130721e1f5f4ea5f72b9ec3e5
Wajahat Ahmed <64039031+Wajahat049@users.noreply.github.com>
2022-03-25 00:22:54 +0500

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@
 // const cheerio = require("cheerio");
 // const pretty = require("pretty");
 // const fs = require('fs');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // let rawdata = fs.readFileSync("E:\\job-search-website\\src\\Assests\\1,144 Computer Jobs in Karachi - 22 March, 2022 _ Indeed.com Pakistan(1).json");
 // let data = JSON.parse(rawdata);
@@ -10,9 +17,6 @@
 // console.log(data[i].Field4);
 // console.log(data[i].Field5);
 // console.log("");
-
-
-
 
 // }
 
@@ -33,25 +37,61 @@
 
 // Scrape()
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-async function getData(){
-
-const uri = "mongodb+srv://wajahat:wajju123@cluster0.0buc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-let result =await client.connect()
-// client.connect(async err =>{
+async function getData() {
+  const uri =
+    "mongodb+srv://wajahat:wajju123@cluster0.0buc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  let result = await client.connect();
+  // client.connect(async err =>{
   let collection = result.db("GoJobber").collection("Users");
-  collection.insertOne({name:"ahmed"}, function(err, res) {
+  // collection.insertOne({name:"ahmed"}, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("1 document inserted");
+  //   // db.close();
+  // });
+  let response = await collection.find({}).toArray();
+  return response;
+  // console.log(response[0].name)
+
+  // })
+}
+
+async function postData(user) {
+  const uri =
+    "mongodb+srv://wajahat:wajju123@cluster0.0buc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  let result = await client.connect();
+  // client.connect(async err =>{
+  let collection = result.db("GoJobber").collection("Users");
+  collection.insertOne(user, function (err, res) {
     if (err) throw err;
     console.log("1 document inserted");
     // db.close();
   });
-//   let response = await collection.find({}).toArray()
-//   console.log(response[0].name)
-  // perform actions on the collection object
-//   client.close();
-// })
 }
-getData()
+
+app.get("/AllUsers", (req, res) => {
+  var resp = getData();
+  resp.then((e) => {
+    res.send(e);
+  });
+});
+
+app.post("/CreateUser", (req, res) => {
+  console.log("New User", req.body);
+  postData(req.body);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on Port ${PORT}`);
+});
