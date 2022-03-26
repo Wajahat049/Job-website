faff89c66efa0002ef6db91336d0f6cbcd195af2
Wajahat Ahmed <64039031+Wajahat049@users.noreply.github.com>
2022-03-26 11:48:32 +0500

    Update index.jsx

--- src/Components/accountBox/index.jsx
+++ src/Components/accountBox/index.jsx
@@ -7,8 +7,8 @@ import { AccountContext } from "./accountContext";
 import { SignupForm } from "./signupForm";
 
 const BoxContainer = styled.div`
-  width: 280px;
-  min-height: 550px;
+  width: 270px;
+  min-height: 495px;
   display: flex;
   flex-direction: column;
   border-radius: 19px;
